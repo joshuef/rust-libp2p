df6309fcad9210ef0e141463cb13bc1efb024665
Josh Wilson <joshuef@gmail.com>
2023-10-27 21:24:00 +0200

    feat(kad): convert kad record.value to Bytes.
    This should help avoid potentially costly clones

--- protocols/kad/src/handler.rs
+++ protocols/kad/src/handler.rs
@@ -24,6 +24,7 @@ use crate::protocol::{
 };
 use crate::record_priv::{self, Record};
 use crate::QueryId;
+use bytes::Bytes;
 use either::Either;
 use futures::prelude::*;
 use futures::stream::SelectAll;
@@ -296,7 +297,7 @@ pub enum HandlerEvent {
         /// The key of the stored record.
         key: record_priv::Key,
         /// The value of the stored record.
-        value: Vec<u8>,
+        value: Bytes,
         /// The user data passed to the `PutValue`.
         query_id: QueryId,
     },
@@ -444,7 +445,7 @@ pub enum HandlerIn {
         /// Key of the value that was put.
         key: record_priv::Key,
         /// Value that was put.
-        value: Vec<u8>,
+        value: Bytes,
         /// Identifier of the request that was made by the remote.
         request_id: RequestId,
     },

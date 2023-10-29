b6bc31c6ef70426048bf17a359dee8203634afa6
Josh Wilson <joshuef@gmail.com>
2023-10-29 13:31:55 +0100

    chore(gossipsub): remove double conversion to Bytes on publish

--- protocols/gossipsub/src/behaviour.rs
+++ protocols/gossipsub/src/behaviour.rs
@@ -607,7 +607,7 @@ where
         topic: impl Into<TopicHash>,
         data: impl Into<Bytes>,
     ) -> Result<MessageId, PublishError> {
-        let data = Bytes::from(data.into());
+        let data: Bytes = data.into();
         // Convert the input topic into TopicHash
         let topic = topic.into();
 
